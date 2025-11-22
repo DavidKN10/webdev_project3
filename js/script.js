df6f8ce77f5ac3e5629da8fa7172ccfb30455b20
DavidKN10 <davidrs23178@gmail.com>
2025-11-21 21:25:57 -0600

    added marker, info window, and traffic to google map

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
-const coordinates = {
+const iitCoordinates = {
   // IIT coordinates 
   lat: 41.8367,
   lng: -87.626
 }
 
+
+
 function initMap() {
-  let map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
-    center: coordinates,
+    center: iitCoordinates,
     mapId: "map_id" 
   });
+
+  const traffic = new google.maps.TrafficLayer().setMap(map);
 
   
   const marker = new google.maps.marker.AdvancedMarkerElement({
     map: map,
-    position: coordinates,
+    position: iitCoordinates,
     title: "Illinois Institute of Technology"
   });
+
+  const info = new google.maps.InfoWindow({
+    content: `<h3>Illinois Institute of Technology</h3>
+              <p>This is the university I go to.</p>
+              <style>h3{color:black}p{color:black}</style>`,
+  });
+
+  marker.addListener("click", () => {
+    info.open({
+      anchor: marker,
+      map
+    });
+  }); 
 }
+
